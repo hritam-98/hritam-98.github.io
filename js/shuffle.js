9004125ff0270587ebe2201df6ed2ca76ee6e57f
Hritam Basak <hritambasak48@gmail.com>
2024-01-21 22:55:00 +0530

    Update shuffle.js

--- js/shuffle.js
+++ js/shuffle.js
@@ -1,5 +1,5 @@
 function shuffle_stanford() {
     shuffleLetters(document.getElementById('stanford_email'), {
-        text: 'paridhi@stanford.edu',
+        text: 'hritam.basak@stonybrook.edu',
     });
 }
